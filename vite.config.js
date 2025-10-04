e7c71c2f042b9ed9182b255de1882c62c32e16ab
Lele97 <gabriele.grandinetti@hotmail.com>
2025-10-04 17:23:00 +0200

    update .env and vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig, loadEnv } from 'vite'
+import {defineConfig, loadEnv} from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig(({ mode }) => {
+export default defineConfig(({mode}) => {
     const env = loadEnv(mode, process.cwd(), '')
 
     return {
@@ -10,11 +10,6 @@ export default defineConfig(({ mode }) => {
             host: env.VITE_HOST,
             port: parseInt(env.VITE_PORT || '8888', 10),
             open: true,
-            ...(mode !== 'development' && {
-                allowedHosts: env.VITE_ALLOWED_HOSTS
-                    ? env.VITE_ALLOWED_HOSTS.split(',')
-                    : ['localhost']
-            }),
             proxy: {
                 '/api': {
                     target: env.VITE_GETAWAY_SERVER_URL,
