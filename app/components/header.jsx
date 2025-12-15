7ea1caee0a72e36ba63514527509fc8f2adc1a62
Lele97 <gabriele.grandinetti@hotmail.com>
2025-12-15 20:39:03 +0100

    update avatar and logiut in header

--- app/components/header.jsx
+++ app/components/header.jsx
@@ -1,25 +1,57 @@
 import React from "react";
-import styles from '~/styles/header.module.css'
-import BuyMeACoffeeButton from '~/components/BuyMeACoffeeButton';
+import styles from "~/styles/header.module.css";
+import BuyMeACoffeeButton from "~/components/BuyMeACoffeeButton";
 
 const Header = ({user, logout}) => {
     return (
         <header className={styles.header}>
             <div className={styles.headerContent}>
-                <BuyMeACoffeeButton></BuyMeACoffeeButton>
+                <BuyMeACoffeeButton/>
+
                 <div className={styles.logoTitleContainer}>
-                    <img src="/pagaTu.png" alt="PagaTu - Logo applicazione caffè aziendale" className={styles.pagatu_image}/>
+                    <img
+                        src="/pagaTu.png"
+                        alt="PagaTu - Logo applicazione caffè aziendale"
+                        className={styles.pagatu_image}
+                    />
                 </div>
+
                 <div className={styles.userInfo}>
-                    <i className="bi bi-person-circle" aria-hidden="true"></i>
-                    <span className={styles.welcomeText}>Ciao {user}</span>
-                    <button onClick={logout} className={styles.logoutBtn} aria-label="Logout" title="Esci dall'applicazione">
-                        <img src="/logout-svgrepo-com.svg" alt="" role="presentation"/>
-                    </button>
+                    <div
+                        className={styles.coffeeAvatar}
+                        aria-label={`Utente ${user}`}>
+                        <img
+                            src="/coffee-cup-coffee-svgrepo-com.png"
+                            alt=""
+                            className={styles.coffeeImg}/>
+                        <span className={styles.coffeeLetter}>
+              {user?.charAt(0).toUpperCase()}
+            </span>
+                    </div>
+
+                    {/*  <button
+                        onClick={logout}
+                        className={styles.logoutBtn}
+                        aria-label="Logout"
+                        title="Esci dall'applicazione">
+                        <img
+                            src="/logout-svgrepo-com.svg"
+                            alt=""
+                            role="presentation"/>
+                    </button>*/}
+
+                    <div className={styles.door_container}>
+                        <div className={styles.top_bar}></div>
+                        <div className={styles.door_frame}></div>
+                        <div className={styles.door_panel}></div>
+                        <div className={styles.doorknob}>
+                            <div className={styles.doorknob_inner}></div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </header>
-    )
-}
+    );
+};
 
 export default Header;
